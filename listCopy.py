a697e914a3382c7bbf16bc42857d3351fddf2cc3
Mehmet Zahid Büyükzengin <zahidbuyukzengin89@gmail.com>
2024-08-21 00:09:57 +0300

    list kopyalama

---
+++ listCopy.py
@@ -0,0 +1,14 @@
+#liste kopyalama copy() methodu
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+#2.kopyalama yolu
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+# : slice operatoru ile 
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
