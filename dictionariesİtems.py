9be69a63abbc8c9c6d8d2c73438e27b3448d3754
Mehmet Zahid Büyükzengin <zahidbuyukzengin89@gmail.com>
2024-08-27 14:49:24 +0300

    Dict item

---
+++ dictionariesİtems.py
@@ -0,0 +1,17 @@
+#bir dict tanımladık ve yearı 2018 e eşitledik
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+print(thisdict)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
+print(thisdict)
