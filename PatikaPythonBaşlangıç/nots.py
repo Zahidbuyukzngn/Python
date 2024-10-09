144d7b619855bd50828986c20827c38bf93fd875
Mehmet Zahid Büyükzengin <zahidbuyukzengin89@gmail.com>
2024-10-10 01:33:26 +0300

    kısa harici notlarım

---
+++ PatikaPythonBaşlangıç/nots.py
@@ -0,0 +1,11 @@
+# Create a function which multiply two numbers
+
+numbers=[1,2,3,4,5,6,7,8,]
+sorted_numbers=sorted(numbers)
+print(numbers)
+
+
+
+#CD .. ile bir geri dosyaya gelir
+#\t ile 4 tane boşluk bırakabiliriz
+print("bugün mecidiye'den kadıköy'\e 1 saatte geldim")
