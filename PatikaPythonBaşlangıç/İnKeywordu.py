061ec5baaf3677abfa73bc8e66add9528fc2eeff
Mehmet Zahid Büyükzengin <zahidbuyukzengin89@gmail.com>
2024-10-12 23:30:16 +0300

    in keywordu kullanımı

---
+++ PatikaPythonBaşlangıç/İnKeywordu.py
@@ -0,0 +1,10 @@
+#Belirli bir eleman listede veya tuple'da var mı diye in keyword'ü ile sorgu yapabilirim.
+#false çıktısı verir çünkü 40 listede yok
+l = [1,2,3,4]
+print(40 in l)
+
+#true çıktısı verir çünkü 2 listede var
+print(2 in l)
+
+
+#else if ile kullanabiliriz 
