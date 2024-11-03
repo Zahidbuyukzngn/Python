5e66cb3c01ae7166d5ef651cc3d171a388fb9796
Mehmet Zahid Büyükzengin <zahidbuyukzengin89@gmail.com>
2024-11-03 23:18:49 +0300

    koşul blokları ile trafik ışığı recap

---
+++ trafikIşığıRecap.py
@@ -0,0 +1,13 @@
+ısıklar=["red","yellow","green"]
+
+hangiısık=ısıklar[2]
+print(hangiısık)
+
+if hangiısık == "red":
+    print("DUR!")
+
+elif hangiısık == "yellow":
+    print("HAZIRLAN!")
+
+else:
+    print("GEÇ")
