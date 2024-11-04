0b55e34a2025ea8aca1f77e7f5c4a068035db75d
Mehmet Zahid Büyükzengin <zahidbuyukzengin89@gmail.com>
2024-11-04 18:18:13 +0300

    0 ve 1 de "asal değildir" yazdırttım

--- enginDemiroğUdemy/asalMıRecap.py
+++ enginDemiroğUdemy/asalMıRecap.py
@@ -1,12 +1,19 @@
-
 while True:
-    sayı=int(input("lütfen sayıyı girin: "))
+    sayı = int(input("Lütfen sayıyı girin: "))
 
-    for i in range(2,sayı):
-        if sayı % i == 0:
-            print(f"{sayı} bir asal sayı değil. Tam böleni: {i}")
-            break
+    if sayı <= 1:
+        print(f"{sayı} bir asal sayı değildir.")
+    else:
+        for i in range(2, sayı):
+            if sayı % i == 0:
+                print(f"{sayı} bir asal sayı değil. Tam böleni: {i}")
+                break
         else:
+            # for döngüsü break yapılmadan biterse sayı asaldır
             print(f"{sayı} bir asal sayıdır.")
-            break
-    devam=input("Tekrardan bir sayı girmek istiyor musunuz? (E/H): ").lower()
+
+    devam = input("Tekrardan bir sayı girmek istiyor musunuz? (E/H): ").lower()
+
+    if devam != "e":
+        print("Programdan çıkılıyor...")
+        break
