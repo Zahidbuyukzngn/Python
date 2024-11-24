5b3331a5bf2de5852014c8d3340066a1a9bf9905
Mehmet Zahid Büyükzengin <zahidbuyukzengin89@gmail.com>
2024-11-24 22:50:52 +0300

    derecemizi özelleştirdik.gösterge çubuğunun nerde olduğunu gösteren kod yazııldı.

--- GuiDersleri/scale.py
+++ GuiDersleri/scale.py
@@ -5,7 +5,18 @@ window =Tk()
 def submit():
     print("the temparature is: "+str(scale.get())+" degrees C")
 
-scale= Scale(window,from_=100,to=0,)
+scale= Scale(window,
+             from_=1000,
+             to=0,
+             length=600,
+             orient=VERTICAL,
+             font=("Consolas",20),
+             tickinterval=10,
+             #showvalue=0,
+             resolution=5,)
+
+scale.set(((scale["from"]-scale["to"])/2)+scale["to"])
+
 scale.pack()
 
 button=Button(window,text="Submit",command=submit)
