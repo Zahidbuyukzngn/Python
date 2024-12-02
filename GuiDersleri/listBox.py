82b3c1112984f69016d6e675ff0cde18ebee97b5
Mehmet Zahid Büyükzengin <zahidbuyukzengin89@gmail.com>
2024-12-02 14:53:11 +0300

    listboxumuzu özelleştirdik ve submit butonu ekleyerek siparişi print ile yazdırdık

--- GuiDersleri/listBox.py
+++ GuiDersleri/listBox.py
@@ -1,8 +1,16 @@
 from tkinter import *
 
+def submit():
+    print("You have ordered: ")
+    print(listbox.get(listbox.curselection()))
+
+
 window=Tk()
 
 listbox=Listbox(window,
+                bg=("#f7ffde"),
+                font=("constantia",35),
+                width=12
                 )
 
 listbox.pack()
@@ -13,7 +21,12 @@ listbox.insert(3,"Hamburger")
 listbox.insert(4,"MeatBread")
 listbox.insert(5,"Soup")
 
+listbox.config(height=listbox.size())
 
+button=Button(window,
+              text="submit",
+              command=submit)
 
+button.pack()
 
 window.mainloop()
